61651b5f5dee7b6159c08081dd2890f0fb295ade
amirmahmoudi1967 <47902830+amirmahmoudi1967@users.noreply.github.com>
2020-12-03 15:03:31 +0100

    Create main.cs

---
+++ Exercice 1/main.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Queue<string> l = new Queue<string>();
+		
+		l.Enqueue(new Node<string>("Ciao,"));
+		l.Enqueue(new Node<string>("Mondo\n"));
+		l.print();
+		
+		l.Dequeue;
+		l.print();
+	}
+}
