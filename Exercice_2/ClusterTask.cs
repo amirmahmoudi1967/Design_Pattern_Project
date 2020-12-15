07a5a74a0eb49d8ac983c228f48624118f44a957
Amir_mmdi <47902830+amirmahmoudi1967@users.noreply.github.com>
2020-12-15 10:31:59 +0100

    Create ClusterTask.cs

---
+++ Exercice_2/ClusterTask.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+class ClusterTask
+{
+    private int[] array;
+    private Dictionary<int, int> results;
+    public ClusterTask(int[] array)
+    {
+        this.array = array;
+        results = new Dictionary<int, int>();
+    }
+    public void execute()
+    {
+        int rows = array.Length;
+
+        for (int a = 0; a < rows; a++)
+        {
+            int val = array[a];
+
+            if (results.ContainsKey(val))
+            {
+                results[val] = results[val] + 1;
+            }
+            else
+            {
+                results[val] = 1;
+            }
+        }
+    }
+    public Dictionary<int, int> GetResults()
+    {
+        return results;
+    }
+}
